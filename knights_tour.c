b9c9add8606b097bdc41c796bd81ad42814b33f8
Mark Bowden <Mbowden01@gmail.com>
2014-06-23 21:39:45 +0900

    Update knights_tour.c

--- knights_tour.c
+++ knights_tour.c
@@ -3,8 +3,9 @@
 
 /* use backtracking to find first solution */
 int _solve(int x, int y, int** board, int n){
+    /* all possible components of a legal night move */
     const int moves[4] = {-2,-1,1,2};
-    /* check if solution */
+    /* base case: check if solution */
     if(board[x][y] == n * n){
         return 1;
     }
@@ -12,17 +13,18 @@ int _solve(int x, int y, int** board, int n){
     int i,j;
     for(i = 0; i < 4; i++){
         for(j = 0; j < 4; j++){
-            /* moves must contain unique absolute values */
+            /* check for legal knight move */
             if(abs(moves[i]) == abs(moves[j])){
                 continue;
             }
-            /* spot to move must not be used and on board */
+            /* set position to try */
             int newx = x + moves[i];
             int newy = y + moves[j];
+            /* check if move is on the board or into a already used space */
             if(newx < 0 || newx >= n || newy < 0 ||  newy >= n || board[newx][newy] != 0){
                 continue;
             }
-            /* change board */
+            /* set the co-ordinates of the new position to current move + 1 */
             board[newx][newy] = board[x][y] + 1;
             /* recurse and propogate solution if found */
             if( _solve(newx, newy, board, n) ){
@@ -38,20 +40,22 @@ int _solve(int x, int y, int** board, int n){
 
 /* wrapper for solving function */
 int **solve(int n){
+    /* allocate a board */
     int i;
     int ** board = malloc(sizeof(int*) * n);
     for(i = 0; i < n; i++){
         board[i] = malloc(sizeof(int) * n);
     }
-    /* start at 0,0 */
+    /* start at position 0,0 */
     int x = 0;
     int y = 0;
+    /* start move index as 1 (either this or change _solve's base case) */
     board[x][y] = 1;
     _solve(x,y, board, n);
     return board;
 }
 
-/* printer funtion for board aka an array */
+/* simple printer funtion for board aka an array */
 void prar(int ** ar, int n){
     int i,j;
     for(i = 0; i < n; i++){
